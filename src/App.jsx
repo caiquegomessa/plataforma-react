a177add12302456d5eec0893d5d6f7f13cc18ff2
caiquegomessa <caiquegomessa@gmail.com>
2023-03-21 14:17:55 -0300

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Menu } from './components/Menu/Menu';
+import { Usuarios } from './pages/Usuarios/Usuarios';
+import {Blog} from "./pages/Blog/Blog"
+import { Contato } from './pages/Contato/Contato';
+import { Home } from './pages/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+      <BrowserRouter>
+      <Menu/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/contato" element={<Contato/>}/>
+          <Route path="/usuarios" element={<Usuarios/>}/>
+        </Routes>
+      </BrowserRouter>   
+      </div>
   );
 }
 
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/contato">Contato</Link>
+                </li>
+                <li>
+                    <Link to="/usuarios">Usuários</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
